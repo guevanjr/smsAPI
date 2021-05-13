d5379edecc613d7c90e1c75c0f52c4d0a95c047d
Simião Guevan Jr <guevanjr@users.noreply.github.com>
2021-05-13 22:56:35 +0200

    Change API response format to JSON and add IP validation

--- controllers/public.controller.js
+++ controllers/public.controller.js
@@ -1,83 +1,45 @@
-// Includes crypto module
-/*
-const crypto = require('crypto');
-  
-// Defining algorithm
-const algorithm = 'aes-256-cbc';
-  
-// Defining key
-const key = crypto.randomBytes(32);
-  
-// Defining iv
-const iv = crypto.randomBytes(16);
-  
-// An encrypt function
-function encrypt(apiKey) {
-    // Creating Cipheriv with its parameter
-    let cipher = crypto.createCipheriv(algorithm, Buffer.from(key), iv);
-    
-    // Updating text
-    let encrypted = cipher.update(apiKey);
-    
-    // Using concatenation
-    encrypted = Buffer.concat([encrypted, cipher.final()]);
-    
-    // Returning iv and encrypted data
-    return { 
-        iv: iv.toString('hex'),
-        encryptedData: encrypted.toString('hex') 
-    };
-}
-  
-// A decrypt function
-function decrypt(apiToken) {
-    let iv = Buffer.from(apiToken.iv, 'hex');
-    let encryptedText = Buffer.from(apiToken.encryptedData, 'hex');
-    
-    // Creating Decipher
-    let decipher = crypto.createDecipheriv(algorithm, Buffer.from(key), iv);
-    
-    // Updating encrypted text
-    let decrypted = decipher.update(encryptedText);
-    decrypted = Buffer.concat([decrypted, decipher.final()]);
-    
-    // returns data after decryption
-    return decrypted.toString();
-}
-  
-// Encrypts output
-var output = encrypt("819f368f-3212-4cd1-ad27-fb0852cde6cb");
-//console.log(output);
-  
-// Decrypts output
-//console.log(decrypt(output));
-*/
-
-var smpp = require('smpp');
+const smpp = require('smpp');
 var session = new smpp.Session({
     host: '197.218.5.7', 
     port: 8805,
-    auto_enquire_link_period: 100000
+    auto_enquire_link_period: 300000
 });
 
-/*
+
 const { Pool } = require('pg')
 const pool = new Pool({
   user: 'adem',
-  host: 'localhost',
+  host: '162.214.149.184',
   database: 'adem',
   password: 'AdeM123',
   port: 5432
 })
-*/
 
-var smsId = '';
-var smsStatus = '';
+function updateSMSLogs(messageId, phoneNumber, senderId, messageStatus, messageSource, messageType, messageText) { 
+    var curDateTime = new Date(Date.now()).toISOString().replace('T',' ').substr(0, 19);
+    console.log(curDateTime);
 
-// We will track connection state for re-connecting
+    var sqlText = "INSERT INTO sms_logs(messageid, fromnumber, tonumber, status, datetime, source, type, operator, message) VALUES('" + 
+        messageId + "','" + 
+        phoneNumber.replace('+','') + "','" +
+        senderId.replace('+','') + "','" +
+        messageStatus + "','" +
+        curDateTime + "','" +
+        messageSource + "','" +
+        messageType + 
+        "','Vodacom','" + 
+        messageText + "');";
+
+    //client.RPUSH(smsId, text, fromNumber, toNumber);
+    pool.query(sqlText, (err, res) => {
+        console.log(err, res/*.rowCount*/) 
+        //pool.end() 
+    });
+}
+
 var didConnect = false; 
 
-session.on('connect', function(){
+session.on('connect', function() {
   didConnect = true;
 
   session.bind_transceiver({
@@ -88,7 +50,7 @@ session.on('connect', function(){
   }, function(pdu) {
     console.log('pdu status', lookupPDUStatusKey(pdu.command_status));
     if (pdu.command_status == 0) {
-        console.log('Movitel SMPP Successfully Bound')
+        console.log('Movitel BrandName SMPP Successfully Bound')
     }
   });
 })
@@ -104,7 +66,11 @@ function lookupPDUStatusKey(pduCommandStatus) {
 function connectSMPP() {
     console.log('Movitel BrandName SMPP Reconnecting');
     session.connect();
-    session = new smpp.Session({host: '197.218.5.7', port: 8805});
+    session = new smpp.Session({
+        host: '197.218.5.7', 
+        port: 8805,
+        auto_enquire_link_period: 300000
+    });
 }
   
 session.on('close', function(){
@@ -127,11 +93,19 @@ function sleep(milliseconds) {
 
 function isHostValid(origin) {
     let isValid = false;
-
+    let i = 0;
+    let ipArray = [
+        '192.168.30.119',   // SAP Produtivo
+        '192.168.30.121',   // SAP Qualidade
+        '197.219.113.211',   // My Laptop @ ITGEST network
+        '197.237.202.144'   // My Laptop @ Huawei Mate Hotspot (via Vodacom)
+    ]
     console.log('Host: ' + origin);
 
-    if(origin.startsWith('192.168.')) {
-        isValid = true;
+    for(i == 0; i < ipArray.length; i++) {
+        if(origin == ipArray[i]) {
+            isValid = true;
+        }
     }
 
     return isValid;
@@ -143,9 +117,10 @@ function isTokenValid(apiToken) {
     let tokenString = Buffer.from(apiToken.replace('Bearer ', ''), 'base64');
     let decodedToken = tokenString.toString('utf-8');
     let tokenArray = decodedToken.split('|');
-    console.log(decodedToken);
-    console.log('Username: ' + tokenArray[0]);
-    console.log('API Key: ' + tokenArray[1]);
+
+    //console.log(decodedToken);
+    //console.log('Username: ' + tokenArray[0]);
+    //console.log('API Key: ' + tokenArray[1]);
 
     if(tokenArray[0] == 'sap_sms360' && tokenArray[1] == 'kdlAsudl3xerg90ed') {
         isValid = true;
@@ -154,39 +129,85 @@ function isTokenValid(apiToken) {
     return isValid;
 }
 
-function sendSMS(from, to, text, source, type) {
-    let smsFrom = from;
-    let smsTo   = to;
-    let smsText = text;
-    let smsSource = source;
-    let smsType = type;
+function jsonResponse(response) {
+    let toJSON = {
+        id: [],
+        text: []   
+    };
+  
+    let statusArray = response;
+  
+    toJSON.id.push(statusArray[0]);
+    toJSON.text.push(statusArray[1]);
+  
+    return toJSON;
+}
 
+function validateParameters(phoneNumber, senderNumber, messageText, messageType, messageSource) {
+    let invalidCount = 0;
+
+    if(phoneNumber == null || phoneNumber == undefined) {
+        invalidCount++;
+    }
+
+    if(senderNumber == null || senderNumber == undefined) {
+        invalidCount++;
+    }
+
+    if(messageText == null || messageText == undefined) {
+        invalidCount++;
+    }
+
+    if(messageType == null || messageType == undefined) {
+        invalidCount++;
+    }
+
+    if(messageSource == null || messageSource == undefined) {
+        invalidCount++;
+    }
+
+    return invalidCount;
+}
+
+async function sendSMS (from, to, text, source, type) {       
+    let smsStatus = '';
+    //let results = await Promise.all([
     session.submit_sm({
-        source_addr: smsFrom, 
-        destination_addr: smsTo, // this is very important so make sure you have included + sign before ISD code to send sms
-        short_message: smsText,
+        source_addr: from, 
+        destination_addr: to, // this is very important so make sure you have included + sign before ISD code to send sms
+        short_message: text,
         source_addr_ton: 5,
         source_addr_npi: 1,
         registered_delivery: 1
-    }, async function(pdu) {
-        console.log('SMS Submit PDU Status: ', lookupPDUStatusKey(pdu.command_status));
-        
-        if (pdu.command_status == 0) {
+    }, async function (pdu) {
+        let smsCode = pdu.message_id;
+        let pduStatus = await pdu.command_status;
+        console.log('1. PDU Status: ' + pduStatus);
+
+        if (pduStatus == 0) {
             // Message successfully submitted
-            smsId = pdu.message_id;
-            smsStatus = 'SEND_SUCCESS';
+            updateSMSLogs(smsCode, smsFrom, smsTo, smsStatus, smsSource, smsType, smsText);
+            console.log('2. SMS ID: ' + smsCode);
+            //console.log('2. ' + smsStatus.concat(': ', smsTo));
+            smsStatus = 'MSG900|SEND_SUCCESS';
+            return smsStatus;
         } else {
             // Message submission Error
-            smsStatus = 'SEND_FAILED - Error Code: ' + pdu.command_status;
+            smsStatus = 'MSG' + pduStatus + '|ERR_NOT_SENT';
+            return smsStatus;
         }
-
-    });
-
-      return smsStatus;
+    }) 
+//])
+    //console.log('3. PDU Status: ' + send);
+    return await smsStatus;
 }
 
 session.on('pdu', function(pdu){
     // incoming SMS from SMSC
+    if(pdu.command == 'submit_sm') {
+        console.log('0. Enviado')
+    }
+
     if (pdu.command == 'deliver_sm') {
         
         // no '+' here
@@ -194,14 +215,18 @@ session.on('pdu', function(pdu){
         //var fromNumber = pdu.destination_addr.toString();
         
         var text = '';
+        var responseStatus = '';
+
         if (pdu.short_message && pdu.short_message.message) {
             text = pdu.short_message.message;
-            smsStatus = text.split(' ');
-            console.log('Status: ' + smsStatus[5]);
+            responseStatus = text.split(' ');
+            console.log('Status: ' + responseStatus[5]);
         }
                
         // Reply to SMSC that we received and processed the SMS
-        session.deliver_sm_resp({ sequence_number: pdu.sequence_number });
+        session.deliver_sm_resp({ 
+            sequence_number: pdu.sequence_number 
+        });
     }
 })
 
@@ -222,14 +247,17 @@ session.on('enquire_link_resp', function (pdu) {
 
 
 exports.singleSMS = async function(req, res, id) {
-    //return res.status(200).send("TEST_SUCCESS");
-    if (req.body.number == null || req.body.number == undefined) {
+    let status = 'null|null'
+    let invalidCount = validateParameters(req.body.number, req.body.from, req.body.text, req.body.type, req.body.source);
+
+    if (invalidCount > 0) {
         //No unsent SMS found
-        return res.status(400).send('SEND_FAILED - Invalid or Incomplete Parameters');
+        status = 'MSG901|ERR_NULL_PARAM';
+        return res.status(400).send(jsonResponse(status.split('|')));
     } else {
-        //console.log(req)
         let reqToken = req.headers.authorization;
         let reqHost = req.headers.origin;
+        let smsStatus = '';
 
         if(isTokenValid(reqToken) && isHostValid(reqHost)) {
             let smsSource = req.body.source;
@@ -237,13 +265,42 @@ exports.singleSMS = async function(req, res, id) {
             let smsTo = req.body.number;
             let smsFrom = req.body.from;
             let smsType = req.body.type;
+            let smsCode = '';
+            console.log(req.headers.referer);
 
-            let status = sendSMS(smsFrom, smsTo, smsText, smsSource, smsType);
-            console.log('SMS to ' + smsTo + ': ' + status);
-            return res.status(200).send(status);
+            session.submit_sm({
+                source_addr: smsFrom, 
+                destination_addr: smsTo, // this is very important so make sure you have included + sign before ISD code to send sms
+                short_message: smsText,
+                source_addr_ton: 5,
+                source_addr_npi: 1,
+                registered_delivery: 1
+            }, async function (pdu) {
+                smsCode = pdu.message_id;
+                let pduStatus = await pdu.command_status;
+                console.log('1. PDU Status: ' + pdu.command_status);
+        
+                if (pduStatus == 0) {
+                    // Message successfully submitted
+                    status = 'MSG900|SEND_SUCCESS';
+                    smsStatus = 'SEND_SUCCESS'
+                    return res.status(200).send(jsonResponse(status.split('|')));;
+                } else {
+                    // Message submission Error
+                    status = 'MSG' + pduStatus + '|ERR_NOT_SENT';
+                    smsStatus = 'ERR_NOT_SENT';
+                    return res.status(400).send(jsonResponse(status.split('|')));;
+                }
+            })
+
+            updateSMSLogs(smsCode, smsFrom, smsTo, smsStatus, smsSource, smsType, smsText);
+            //return res.status(200).send(status);
         } else {
-            return res.status(400).send('ERR_TOKEN_HOST');
+            status = 'MSG902|ERR_TOKEN_HOST';
+            smsStatus = 'ERR_TOKEN_HOST';
+            return res.status(400).send(jsonResponse(status.split('|')));
         }
+
     }
 };
 
--- controllers/vodacom.controller.js
+++ controllers/vodacom.controller.js
@@ -16,11 +16,6 @@ const pool = new Pool({
 })
 
 var smsId = '';
-var smsStatus = '';
-
-//client.on('connect', function() {
-//console.log('Redis Connected ...')
-//})
 
 // We will track connection state for re-connecting
 var didConnect = false; 
@@ -73,11 +68,19 @@ function lookupPDUStatusKey(pduCommandStatus) {
 
 function isHostValid(origin) {
   let isValid = false;
-
+  let i = 0;
+  let ipArray = [
+      '192.168.30.119',   // SAP Produtivo
+      '192.168.30.121',   // SAP Qualidade
+      '197.219.113.211',   // My Laptop @ ITGEST network
+      '197.237.202.144'   // My Laptop @ Huawei Mate Hotspot (via Vodacom)
+  ]
   console.log('Host: ' + origin);
 
-  if(origin.startsWith('192.168.')) {
-      isValid = true;
+  for(i == 0; i < ipArray.length; i++) {
+      if(origin == ipArray[i]) {
+          isValid = true;
+      }
   }
 
   return isValid;
@@ -89,9 +92,10 @@ function isTokenValid(apiToken) {
   let tokenString = Buffer.from(apiToken.replace('Bearer ', ''), 'base64');
   let decodedToken = tokenString.toString('utf-8');
   let tokenArray = decodedToken.split('|');
-  console.log(decodedToken);
-  console.log('Username: ' + tokenArray[0]);
-  console.log('API Key: ' + tokenArray[1]);
+
+  //console.log(decodedToken);
+  //console.log('Username: ' + tokenArray[0]);
+  //console.log('API Key: ' + tokenArray[1]);
 
   if(tokenArray[0] == 'sap_sms360' && tokenArray[1] == 'kdlAsudl3xerg90ed') {
       isValid = true;
@@ -100,12 +104,53 @@ function isTokenValid(apiToken) {
   return isValid;
 }
 
-function sendSMS(from, to, text, source, type) {  
+function jsonResponse(response) {
+  let toJSON = {
+      id: [],
+      text: []   
+  };
+
+  let statusArray = response;
+
+  toJSON.id.push(statusArray[0]);
+  toJSON.text.push(statusArray[1]);
+
+  return toJSON;
+}
+
+function validateParameters(phoneNumber, senderNumber, messageText, messageType, messageSource) {
+  let invalidCount = 0;
+
+  if(phoneNumber == null || phoneNumber == undefined) {
+      invalidCount++;
+  }
+
+  if(senderNumber == null || senderNumber == undefined) {
+      invalidCount++;
+  }
+
+  if(messageText == null || messageText == undefined) {
+      invalidCount++;
+  }
+
+  if(messageType == null || messageType == undefined) {
+      invalidCount++;
+  }
+
+  if(messageSource == null || messageSource == undefined) {
+      invalidCount++;
+  }
+
+  return invalidCount;
+}
+
+async function sendSMS(from, to, text, source, type) {  
     let smsFrom = from;
     let smsTo   = '+'.concat(to);
     let smsText = text;
     let smsSource = source;
     let smsType = type;
+    let smsStatus = '';
 
     session.submit_sm({
         source_addr: smsFrom, 
@@ -118,17 +163,17 @@ function sendSMS(from, to, text, source, type) {
         if (pdu.command_status == 0) {
             // Message successfully submitted
             smsId = pdu.message_id;
-            smsStatus = 'SEND_SUCCESS';
+            smsStatus = 'ERR_900|SEND_SUCCESS';
         } else {
             // Message submission Error
-            smsStatus = 'SEND_FAILED - Error Code: ' + pdu.command_status;
+            smsStatus = 'ERR_' + pdu.command_status + '|ERR_NOT_SENT';
         }
 
         updateSMSLogs(smsId, smsFrom, smsTo, smsStatus, smsSource, smsType, smsText);
         console.log(smsStatus.concat(': ', smsTo));
       });
 
-      return smsStatus;
+      return jsonResponse(smsStatus.split('|'));
 }
 
 function updateSMSLogs(messageId, phoneNumber, senderId, messageStatus, messageSource, messageType, messageText) { 
@@ -156,23 +201,24 @@ function updateSMSLogs(messageId, phoneNumber, senderId, messageStatus, messageS
 session.on('pdu', function(pdu){
   // incoming SMS from SMSC
   if (pdu.command == 'deliver_sm') {
-    
-    // no '+' here
     var toNumber = pdu.source_addr.toString();
     var fromNumber = pdu.destination_addr.toString();
-    
+    var responseStatus = '';
+
     var text = '';
     if (pdu.short_message && pdu.short_message.message) {
         text = pdu.short_message.message;
-        smsStatus = text.split(' ');
-        console.log('Status: ' + smsStatus[7]);
+        responseStatus = text.split(' ');
+        console.log('Status: ' + responseStatus[7]);
     }
     
-    updateSMSLogs(pdu.message_id, fromNumber.replace('+',''), toNumber.replace('+',''), smsStatus[7].replace('stat:', '').trim(), 'VODACOM_SMSC', 'DELIVRY_MSG', text);
-    console.log('Vodacom SMS From ' + fromNumber + ' To ' + toNumber + ': ' + text);
+    updateSMSLogs(pdu.message_id, fromNumber.replace('+',''), toNumber.replace('+',''), responseStatus[7].replace('stat:', '').trim(), 'VODACOM_SMSC', 'DELIVRY_MSG', text);
+    //console.log('Vodacom SMS From ' + fromNumber + ' To ' + toNumber + ': ' + text);
   
     // Reply to SMSC that we received and processed the SMS
-    session.deliver_sm_resp({ sequence_number: pdu.sequence_number });
+    session.deliver_sm_resp({ 
+      sequence_number: pdu.sequence_number 
+    });
   }
 })
 
@@ -196,7 +242,7 @@ exports.ussdSMS = async function (req, res, id) {
 
     if (req.body.number == null || req.body.number == undefined) {
         //No unsent SMS found
-        return res.status(404).send('Nenhum registo encontrado!');
+        return res.status(400).send('Nenhum registo encontrado!');
     } else {
         let smsSource = req.body.source;
         let smsText = req.body.text;
@@ -211,27 +257,62 @@ exports.ussdSMS = async function (req, res, id) {
 };
 
 exports.apiSMS = async function (req, res, id) {
-  if (req.body.number == null || req.body.number == undefined) {
-      //No unsent SMS found
-      return res.status(400).send('ERR_NULL_PARAM');
-  } else {
-      let reqToken = req.headers.authorization;
-      let reqHost = req.headers.origin;
+    let status = 'null|null'
+    let invalidCount = validateParameters(req.body.number, req.body.from, req.body.text, req.body.type, req.body.source);
 
-      if(isTokenValid(reqToken) && isHostValid(reqHost)) {
-          let smsSource = req.body.source;
-          let smsText = req.body.text;
-          let smsTo = req.body.number;
-          let smsFrom = req.body.from;
-          let smsType = req.body.type;
+    if (invalidCount > 0) {
+        //No unsent SMS found
+        status = 'MSG901|ERR_NULL_PARAM';
+        return res.status(400).send(jsonResponse(status.split('|')));
+    } else {
+        let reqToken = req.headers.authorization;
+        let reqHost = req.headers.origin;
+        let smsStatus = '';
 
-          let status = sendSMS(smsFrom, smsTo, smsText, smsSource, smsType);
-          console.log('SMS to ' + smsTo + ': ' + status);
-          return res.status(200).send(status);
-      } else {
-          return res.status(400).send('ERR_TOKEN_HOST');
-      }
-  }
+        if(isTokenValid(reqToken) && isHostValid(reqHost)) {
+            let smsSource = req.body.source;
+            let smsText = req.body.text;
+            let smsTo = req.body.number;
+            let smsFrom = req.body.from;
+            let smsType = req.body.type;
+            let smsCode = '';
+            console.log(req.headers.referer);
+
+            session.submit_sm({
+                source_addr: smsFrom, 
+                destination_addr: smsTo, // this is very important so make sure you have included + sign before ISD code to send sms
+                short_message: smsText,
+                source_addr_ton: 5,
+                source_addr_npi: 1,
+                registered_delivery: 1
+            }, async function (pdu) {
+                smsCode = pdu.message_id;
+                let pduStatus = await pdu.command_status;
+                console.log('1. PDU Status: ' + pduStatus);
+        
+                if (pduStatus == 0) {
+                    // Message successfully submitted
+                    //console.log('2. SMS ID: ' + smsCode);
+                    //console.log('2. ' + smsStatus.concat(': ', smsTo));
+                    status = 'MSG900|SEND_SUCCESS';
+                    smsStatus = 'SEND_SUCCESS'
+                    return res.status(200).send(jsonResponse(status.split('|')));;
+                } else {
+                    // Message submission Error
+                    status = 'MSG' + pduStatus + '|ERR_NOT_SENT';
+                    smsStatus = 'ERR_NOT_SENT';
+                    return res.status(400).send(jsonResponse(status.split('|')));;
+                }
+            })
+
+            updateSMSLogs(smsCode, smsFrom, smsTo, smsStatus, smsSource, smsType, smsText);
+            //return res.status(200).send(status);
+        } else {
+            status = 'MSG902|ERR_TOKEN_HOST';
+            smsStatus = 'ERR_TOKEN_HOST';
+            return res.status(400).send(jsonResponse(status.split('|')));
+        }
+    }
 };
 
 exports.sendText = async function (req, res, id) {
